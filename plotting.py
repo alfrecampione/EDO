813ff0e85ae62ac083fd1254e2a296780e25eae1
Campione <elmejorasasinodel2016@gmail.com>
2022-12-17 17:47:03 -0500

    Implemented improved euler method

--- plotting.py
+++ plotting.py
@@ -1,2 +1,8 @@
 from project import *
 import matplotlib.pyplot as plt
+
+s, i, r = get_results(100, 2, 10, 1)
+plt.plot(s)
+plt.plot(i)
+plt.plot(r)
+plt.show()
--- project.py
+++ project.py
@@ -4,31 +4,47 @@
 # R = removidos    => r = R/N
 
 ALPHA = 1
+# s' = -s(t)i(t)
+s_der = lambda x, y, z: -x * y
+# i' = s(t)i(t) - ALPHA*i(t)
+i_der = lambda x, y, z: x * y - ALPHA * y
+# r' = ALPHA*i(t)
+r_der = lambda x, y, z: ALPHA * y
 
 
-def get_results(s0, i0, h, n):
+def improved_euler_method(f, g, j, x0: float, y0: float, z0: float, h):
+    u1 = x0 + h * f(x0, y0, z0)
+    v1 = y0 + h * g(x0, y0, z0)
+    w1 = z0 + h * j(x0, y0, z0)
+
+    x1 = x0 + (h / 2) * (f(x0, y0, z0) + f(u1, v1, w1))
+    y1 = y0 + (h / 2) * (g(x0, y0, z0) + g(u1, v1, w1))
+    z1 = z0 + (h / 2) * (j(x0, y0, z0) + j(u1, v1, w1))
+    return x1, y1, z1
+
+
+def runge_kutta_method(f, g, j, x0: float, y0: float, z0: float, h):
+    return
+
+
+def get_results(s0, i0, t_max, h, f=improved_euler_method):
+    n = round(t_max / h)
     r0 = 0
     if s0 <= 0 or i0 <= 0:
         raise Exception("Argument Exception")
-    # s' = -s(t)i(t)
-    s_der = lambda x, y: -x * y
-    # i' = s(t)i(t) - ALPHA*i(t)
-    i_der = lambda x, y: x * y - ALPHA * y
-    # r' = ALPHA*i(t)
-    r_der = lambda x: ALPHA * x
     s = [s0]
     i = [i0]
     r = [r0]
     for j in range(n):
-        if s0 == 0 or i0 == 0:
-            break
-        s1 = s0 + h * s_der(s0, i0)
-        i1 = i0 + h * i_der(s0, i0)
-        r1 = r0 + h * r_der(i0)
+        s1, i1, r1 = f(s_der, i_der, r_der, s0, i0, r0, h)
+        if s1 < 10 ** (-4):
+            s1 = 0
+        if i1 < 10 ** (-4):
+            i1 = 0
+        if r1 < 10 ** (-4):
+            r1 = 0
         s.append(s1)
         i.append(i1)
         r.append(r1)
-        s0 = s1
-        i0 = i1
-        r0 = r1
+        s0, i0, r0 = s1, i1, r1
     return s, i, r
