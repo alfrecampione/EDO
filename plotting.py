e6fc7a50947af775d20b7ba98419b26abe560c5f
Campione <elmejorasasinodel2016@gmail.com>
2022-12-19 10:12:30 -0500

    set a plotting

--- plotting.py
+++ plotting.py
@@ -1,9 +1,18 @@
 import project
 import matplotlib.pyplot as plt
 
-s, i, r = project.get_results(100, 2, 10, 0.01, f=project.runge_kutta_method)
-plt.plot(s)
-plt.plot(i)
-plt.plot(r)
+time_max = 15
+precision = 0.01
+s, i, r = project.get_results(200, 2, time_max, precision, f=project.runge_kutta_method)
+t = [0.0]
+n = int(time_max / precision)
+t0 = 0
+for j in range(n):
+    t1 = t0 + precision
+    t.append(t1)
+    t0 = t1
+plt.plot(t, s)
+plt.plot(t, i)
+plt.plot(t, r)
 # plt.savefig("photo.jpg")
 plt.show()
